2470284d90f10ec77c75fc6e8db02b78450f3920
Evgeny Samsonov <bitniks@yandex.ru>
2019-05-26 17:44:25 +0300

    Вынести данные в main

--- algorithms/dijkstra-algorithm.go
+++ algorithms/dijkstra-algorithm.go
@@ -6,11 +6,6 @@ import (
 )
 
 func main() {
-	dijkstrasAlgorithm();
-}
-
-func dijkstrasAlgorithm() {
-	// Строим взвешенный ацикличный граф
 	graph := make(map[string]map[string]int)
 	graph["start"] = map[string]int{}
 	graph["start"]["a"] = 6
@@ -23,30 +18,35 @@ func dijkstrasAlgorithm() {
 	graph["b"]["a"] = 3
 	graph["b"]["finish"] = 5
 
-	graph["finish"] = map[string]int{} 	// Конечная точка не должна иметь соседей
+	graph["finish"] = map[string]int{}
 
-	// Стоимость переходов
+	costs, parents := findShortestPath(graph, "start", "finish")
+	fmt.Println(costs, parents)
+}
+
+// Finds shortest path using dijkstra algorithm
+func findShortestPath(graph map[string]map[string]int, startNode string, finishNode string) (map[string]int, map[string]string)  {
 	costs := make(map[string]int)
-	costs["a"] = 6
-	costs["b"] = 2
-	costs["finish"] = math.MaxInt32
+	costs[finishNode] = math.MaxInt32
 
-	// Родители
 	parents := make(map[string]string)
-	parents["a"] = "start"
-	parents["b"] = "start"
-	parents["finish"] = ""
+	parents[finishNode] = ""
 
-	// Обработанные узлы
 	processed := make(map[string]bool)
+
+	// Initialization of costs and parents
+	for node, cost := range graph[startNode] {
+		costs[node] = cost
+		parents[node] = startNode
+	}
 
 	lowestCostNode := findLowestCostNode(costs, processed)
 	for ; lowestCostNode != "" ; {
-		// Считает цену для всех соседей узла
+		// Calculation costs for neighbours
 		for node, cost := range graph[lowestCostNode] {
 			newCost := costs[lowestCostNode] + cost
-			// Если цена меньше предыдущей, то обновим
 			if newCost < costs[node] {
+				// Set new cost for this node
 				costs[node] = newCost
 				parents[node] = lowestCostNode
 			}
@@ -56,16 +56,13 @@ func dijkstrasAlgorithm() {
 		lowestCostNode = findLowestCostNode(costs, processed)
 	}
 
-	fmt.Println(costs)
-	fmt.Println(parents)
+	return costs, parents
 }
 
-// Поиск элемента с минимальной ценой
 func findLowestCostNode(costs map[string]int, processed map[string]bool) string {
 	lowestCost := math.MaxInt32
 	lowestCostNode := ""
 	for node, cost := range costs {
-		// Можно инициализировать переменные в if
 		if _, inProcessed := processed[node]; cost < lowestCost && !inProcessed {
 			lowestCost = cost
 			lowestCostNode = node
