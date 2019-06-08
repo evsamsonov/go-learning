db283034b07c9529e45ed3394f5d9467ae9242bf
Evgeny Samsonov <bitniks@yandex.ru>
2019-06-08 22:46:53 +0300

    Самая длинная общая подстрока

--- algorithms/longest-common-sequence.go
+++ algorithms/longest-common-sequence.go
@@ -5,6 +5,9 @@ import "fmt"
 func main() {
 	result := findLongestCommonSequenceSize("fish", "fosh")
 	fmt.Println(result)
+
+	result = findLongestCommonSubstringSize("fish", "fosh")
+	fmt.Println(result)
 }
 
 // Если переменные одного типа в параметрах, то можно указать тип единожды
@@ -27,6 +30,32 @@ func findLongestCommonSequenceSize(a, b string) int {
 	}
 
 	return matrix[len(a) - 1][len(b) - 1]
+}
+
+func findLongestCommonSubstringSize(a, b string) int {
+	matrix := createMatrix(len(a), len(b))
+	lastRowIndex := 0
+	lastColumnIndex := 0
+
+	for i := 0; i < len(a); i++ {
+		for j := 0; j < len(b); j++ {
+			if a[i] == b[j] {
+				if i == 0 || j == 0 {
+					matrix[i][j] = 1
+					continue
+				}
+
+				matrix[i][j] = matrix[i - 1][j - 1] + 1
+
+				lastRowIndex = i
+				lastColumnIndex = j
+			} else {
+				matrix[i][j] = 0
+			}
+		}
+	}
+
+	return matrix[lastRowIndex][lastColumnIndex]
 }
 
 func createMatrix(rows, columns int) [][]int {
