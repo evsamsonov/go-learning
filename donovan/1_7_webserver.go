3dd6e87e26ec1f6ae8d352473dd3dd2e9be07d86
Evgeny Samsonov <bitniks@yandex.ru>
2019-06-24 19:08:15 +0300

    Вебсервер

---
+++ donovan/1_7_webserver.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request)  {
+	// %q - экранированный символьный литерал
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
