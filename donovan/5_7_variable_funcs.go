70e7b67a4997e49eaf0c3238ef9d809ede2beb5c
Evgeny Samsonov <bitniks@yandex.ru>
2019-08-14 17:29:17 +0300

    Вариативные функции

---
+++ donovan/5_7_variable_funcs.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(sum())
+	fmt.Println(sum(1, 2, 3))	// При вызове создается массив и срез на весь массив, который передается в функцию
+
+	numbers := []int{4, 5, 6}
+	fmt.Println(sum(numbers...))		// Если параметры уже в срезе
+}
+
+func sum(values ...int) int {
+	sum := 0
+	for _, val := range values {
+		sum += val
+	}
+
+	return sum
+}
