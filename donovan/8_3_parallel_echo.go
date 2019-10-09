81061c29ce7538699a97e2a7cec532685314a1ca
Evgeny Samsonov <bitniks@yandex.ru>
2019-10-09 16:04:14 +0300

    Параллейный эхо сервер

---
+++ donovan/8_3_parallel_echo.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"time"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8002")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(c net.Conn) {
+	input := bufio.NewScanner(c)
+	for input.Scan() {
+		go echo(c, input.Text(), 2*time.Second)
+	}
+
+	c.Close()
+}
+
+func echo(c net.Conn, shout string, delay time.Duration) {
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+}
