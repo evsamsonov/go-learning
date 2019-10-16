bac2d9ad225c8f65decfd6d5b6c30221dfaa35d8
Evgeny Samsonov <bitniks@yandex.ru>
2019-10-16 17:10:49 +0300

    Тип канала на получение и оправку

--- donovan/8_4_pipeline.go
+++ donovan/8_4_pipeline.go
@@ -6,23 +6,26 @@ func main() {
 	naturals := make(chan int)
 	squares := make(chan int)
 
-	go func() {
-		for i := 0; i < 100; i++ {
-			naturals <- i
-		}
-
-		close(naturals)
-	}()
-
-	go func() {
-		for i := range naturals {
-			squares <- i * i
-		}
-
-		close(squares)
-	}()
+	go counters(naturals)
+	go squarer(naturals, squares)
 
 	for i := range squares {
 		fmt.Println(i)
 	}
+}
+
+func counters(naturals chan<- int) {
+	for i := 0; i < 100; i++ {
+		naturals <- i
+	}
+
+	close(naturals)
+}
+
+func squarer(naturals <-chan int, squares chan<- int) {
+	for i := range naturals {
+		squares <- i * i
+	}
+
+	close(squares)
 }
