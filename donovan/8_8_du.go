789cb1b3890e3222e6e98ec9572f3796845e21ee
Evgeny Samsonov <bitniks@yandex.ru>
2019-11-09 13:45:14 +0300

    Периодический вывод результата

--- donovan/8_8_du.go
+++ donovan/8_8_du.go
@@ -9,6 +9,9 @@ import (
 	"time"
 )
 
+// -v
+var verbose = flag.Bool("v", true, "Вывод промежуточных результатов")
+
 func main() {
 	flag.Parse()
 	roots := flag.Args()
@@ -26,14 +29,37 @@ func main() {
 		close(fileSizes)
 	}()
 
-	var fileCount, fileBytes int64
-	for size := range fileSizes {
-		fileBytes += size
-		fileCount++
+	fmt.Println(*verbose)
+
+	// Периодический вывод результата работы
+	var tick <-chan time.Time
+	if *verbose {
+		tick = time.Tick(500 * time.Millisecond)
 	}
 
-	fmt.Printf("%d files %0.2f GB\n", fileCount, float64(fileBytes)/1e9)
+	var fileCount, fileBytes int64
+loop:
+	for {
+		select {
+		case size, ok := <-fileSizes:
+			if !ok {
+				break loop // указывает, что нужно выйти из цикла, помеченного меткой loop
+			}
+
+			fileBytes += size
+			fileCount++
+		case <-tick:
+			printResult(fileCount, fileBytes)
+		}
+	}
+
+	printResult(fileCount, fileBytes)
+
 	fmt.Println(time.Now())
+}
+
+func printResult(fileCount int64, fileBytes int64) (int, error) {
+	return fmt.Printf("du: %d files %0.2f GB\n", fileCount, float64(fileBytes)/1e9)
 }
 
 func walkDir(dirName string, fileSizes chan<- int64) {
@@ -50,7 +76,7 @@ func walkDir(dirName string, fileSizes chan<- int64) {
 func fetchDirs(dirName string) []os.FileInfo {
 	entries, err := ioutil.ReadDir(dirName)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "du %v\n", err)
+		fmt.Fprintf(os.Stderr, "du: %v\n", err)
 		return nil
 	}
 
