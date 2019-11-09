0af15d5d7d5dbbb16051b506851a5ad1a6541d5a
Evgeny Samsonov <bitniks@yandex.ru>
2019-11-09 14:13:23 +0300

    Параллельный подсчет

--- donovan/8_8_du.go
+++ donovan/8_8_du.go
@@ -6,11 +6,12 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"sync"
 	"time"
 )
 
 // -v
-var verbose = flag.Bool("v", true, "Вывод промежуточных результатов")
+var verbose = flag.Bool("v", false, "Вывод промежуточных результатов")
 
 func main() {
 	flag.Parse()
@@ -22,20 +23,25 @@ func main() {
 	fmt.Println(time.Now())
 
 	fileSizes := make(chan int64)
+
+	var waitGroup sync.WaitGroup
+	for _, root := range roots {
+		waitGroup.Add(1)
+		go walkDir(root, &waitGroup, fileSizes)
+	}
+
 	go func() {
-		for _, root := range roots {
-			walkDir(root, fileSizes)
-		}
+		waitGroup.Wait()
 		close(fileSizes)
 	}()
-
-	fmt.Println(*verbose)
 
 	// Периодический вывод результата работы
 	var tick <-chan time.Time
 	if *verbose {
 		tick = time.Tick(500 * time.Millisecond)
 	}
+
+	fmt.Println("test")
 
 	var fileCount, fileBytes int64
 loop:
@@ -62,18 +68,26 @@ func printResult(fileCount int64, fileBytes int64) (int, error) {
 	return fmt.Printf("du: %d files %0.2f GB\n", fileCount, float64(fileBytes)/1e9)
 }
 
-func walkDir(dirName string, fileSizes chan<- int64) {
+func walkDir(dirName string, waitGroup *sync.WaitGroup, fileSizes chan<- int64) {
+	defer waitGroup.Done()
 	for _, entry := range fetchDirs(dirName) {
 		if entry.IsDir() {
 			subDir := filepath.Join(dirName, entry.Name())
-			walkDir(subDir, fileSizes)
+			waitGroup.Add(1)
+			walkDir(subDir, waitGroup, fileSizes)
 		} else {
 			fileSizes <- entry.Size()
 		}
 	}
 }
 
+// Ограничивающий семафор для ограничений кол-ва открытых файлов
+var fileSemaphore = make(chan struct{}, 20)
+
 func fetchDirs(dirName string) []os.FileInfo {
+	fileSemaphore <- struct{}{}
+	defer func() { <-fileSemaphore }()
+
 	entries, err := ioutil.ReadDir(dirName)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "du: %v\n", err)
