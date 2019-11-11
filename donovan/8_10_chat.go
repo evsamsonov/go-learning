c8d38409a1b2da6d40d3f162e5689db08a88d54e
Evgeny Samsonov <bitniks@yandex.ru>
2019-11-11 21:33:40 +0300

    Чат сервер

--- donovan/8_10_chat.go
+++ donovan/8_10_chat.go
@@ -1,5 +1,75 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+)
 
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go broadcaster()
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+
+		go handleConnChat(conn)
+	}
+}
+
+type client chan<-string		// Канал для исходящих сообщений
+var (
+	entering = make(chan client)
+	leaving = make(chan client)
+	messages = make(chan string)	// Все входящие сообщения клиента
+)
+
+func broadcaster() {
+	clients := make(map[client]bool)		// Все подключенные клиенты
+	for {
+		select {
+		case msg := <-messages:
+			for client := range clients {
+				client <- msg
+			}
+		case client := <-entering:
+			clients[client] = true
+		case client := <-leaving:
+			delete(clients, client)
+			close(client)
+		}
+	}
+}
+
+func handleConnChat(conn net.Conn) {
+	ch := make(chan string)		// Исходящие сообщения клиентов
+	go clientWriter(conn, ch)
+
+	who := conn.RemoteAddr().String()
+	ch <- "Вы " + who
+	messages <- who + " подключился"
+	entering <- ch
+
+	input := bufio.NewScanner(conn)
+	for input.Scan() {
+		messages <- who + ": " + input.Text()
+	}
+
+	leaving <- ch
+	messages <- who + " отключился"
+	conn.Close()
+}
+
+func clientWriter(conn net.Conn, ch <-chan string) {
+	for msg := range ch {
+		fmt.Fprintln(conn, msg)
+	}
 }
