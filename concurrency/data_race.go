018b1d8651af54e9841b942d161d68ce24cd6920
Evgeny Samsonov <bitniks@yandex.ru>
2020-03-01 02:15:46 +0300

    Sync critical section

--- concurrency/data_race.go
+++ concurrency/data_race.go
@@ -1,13 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
 	var num int
+	var mu sync.Mutex
 	go func() {
+		mu.Lock()
 		num++
+		mu.Unlock()
 	}()
+
+	mu.Lock()
 	if num == 0 {
+		fmt.Printf("the value is 0\n")
+	} else {
 		fmt.Printf("the value is %v\n", num)
 	}
+	mu.Unlock()
 }
