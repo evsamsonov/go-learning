7c8fe80d13a85282098a6f8b470e96a9d141e64e
Evgeny Samsonov <bitniks@yandex.ru>
2020-03-08 19:12:38 +0300

    Sync once

---
+++ concurrency/3_sync_once.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var count int
+	increment := func() {
+		count++
+	}
+
+	var once sync.Once
+	var wg sync.WaitGroup
+
+	wg.Add(100)
+	for i := 0; i < 100; i++ {
+		once.Do(increment)
+		wg.Done()
+	}
+
+	wg.Wait()
+
+	fmt.Println(count)
+}
