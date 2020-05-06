37c44245fd5c0c5a2e21757ca4d6087025433b68
Evgeny Samsonov <bitniks@yandex.ru>
2020-05-06 19:43:44 +0300

    Context key type

--- concurrency/4_context_value.go
+++ concurrency/4_context_value.go
@@ -5,17 +5,32 @@ import (
 	"fmt"
 )
 
+type ctxKey int
+
+const (
+	ctxUserID ctxKey = iota
+	ctxAuthToken
+)
+
+func UserID(ctx context.Context) int {
+	return ctx.Value(ctxUserID).(int)
+}
+
+func AuthToken(ctx context.Context) string {
+	return ctx.Value(ctxAuthToken).(string)
+}
+
 func main() {
 	processRequest(1, "123456789")
 }
 
 func processRequest(userID int, token string) {
-	ctx := context.WithValue(context.Background(), "userID", userID)
-	ctx = context.WithValue(ctx, "token", token)
+	ctx := context.WithValue(context.Background(), ctxUserID, userID)
+	ctx = context.WithValue(ctx, ctxAuthToken, token)
 	doProcessRequest(ctx)
 }
 
 func doProcessRequest(ctx context.Context) {
-	fmt.Println(ctx.Value("userID"))
-	fmt.Println(ctx.Value("token"))
+	fmt.Println(AuthToken(ctx))
+	fmt.Println(UserID(ctx))
 }
