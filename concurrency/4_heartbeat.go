67e873d3b857ffe4c11b1c99c58a6d7b689e0c9f
Evgeny Samsonov <bitniks@yandex.ru>
2020-05-18 23:08:19 +0300

    heartbeats

---
+++ concurrency/4_heartbeat.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	doWork := func(
+		done <-chan interface{},
+		pulseInterval time.Duration,
+	) (<-chan interface{}, <-chan time.Time) {
+		heartbeat := make(chan interface{})
+		results := make(chan time.Time)
+
+		go func() {
+			defer close(heartbeat)
+			defer close(results)
+
+			pulse := time.Tick(pulseInterval)
+			workGen := time.Tick(2 * pulseInterval)
+
+			sendPulse := func() {
+				select {
+				case heartbeat <- struct{}{}:
+				default: // Канал heartbeat может никто не слушать
+				}
+			}
+			sendResult := func(r time.Time) {
+				for {
+					select {
+					case <-done:
+						return
+					case <-pulse:
+						sendPulse()
+					case results <- r:
+						return
+					}
+				}
+			}
+			for {
+				select {
+				case <-done:
+					return
+				case <-pulse:
+					sendPulse()
+				case r := <-workGen:
+					sendResult(r)
+				}
+			}
+		}()
+		return heartbeat, results
+	}
+
+	done := make(chan interface{})
+	time.AfterFunc(10*time.Second, func() { close(done) })
+
+	const timeout = 2 * time.Second
+	heartbeat, results := doWork(done, timeout/2)
+	for {
+		select {
+		case _, ok := <-heartbeat:
+			if ok == false {
+				return
+			}
+			fmt.Println("pulse")
+		case r, ok := <-results:
+			if ok == false {
+				return
+			}
+			fmt.Printf("results %v\n", r.Second())
+		case <-time.After(timeout):
+			return
+		}
+	}
+}
