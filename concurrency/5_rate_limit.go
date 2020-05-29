ef2d585b13ce82c2f0bea2612527a2774e9db55e
Evgeny Samsonov <bitniks@yandex.ru>
2020-05-29 22:47:15 +0300

    Rate limiting

---
+++ concurrency/5_rate_limit.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"context"
+	"golang.org/x/time/rate"
+	"log"
+	"os"
+	"sync"
+)
+
+func main() {
+	defer log.Println("Done")
+	log.SetOutput(os.Stdout)
+	log.SetFlags(log.Ltime | log.LUTC)
+
+	apiConnection := Open()
+	var wg sync.WaitGroup
+	wg.Add(20)
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			defer wg.Done()
+			err := apiConnection.ReadFile(context.Background())
+			if err != nil {
+				log.Printf("cannot ReadFile: %v", err)
+			}
+			log.Println("Readfile")
+		}()
+	}
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			defer wg.Done()
+			err := apiConnection.ResolveAddress(context.Background())
+			if err != nil {
+				log.Printf("cannot ReadFile: %v", err)
+			}
+			log.Println("ResolveAddress")
+		}()
+	}
+
+	wg.Wait()
+}
+
+type APIConnection struct {
+	rateLimiter *rate.Limiter
+}
+
+func Open() *APIConnection {
+	return &APIConnection{
+		rateLimiter: rate.NewLimiter(rate.Limit(1), 1),
+	}
+}
+
+func (a *APIConnection) ReadFile(ctx context.Context) error {
+	if err := a.rateLimiter.Wait(ctx); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (a *APIConnection) ResolveAddress(ctx context.Context) error {
+	if err := a.rateLimiter.Wait(ctx); err != nil {
+		return err
+	}
+	return nil
+}
