d7c1e67987e1b7995b91af1aea43109d7a99fbe1
Evgeny Samsonov <bitniks@yandex.ru>
2020-07-31 11:33:30 +0300

    False sharing cache

---
+++ misc/false_sharing_test.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"runtime"
+	"sync"
+	"testing"
+)
+
+const loopLen = 1000
+
+type SimpleStruct struct {
+	n int
+}
+
+type PaddedStruct struct {
+	n int
+	_ CacheLinePad
+}
+
+const CacheLinePadSize = 64
+
+type CacheLinePad struct {
+	_ [CacheLinePadSize]byte
+}
+
+func BenchmarkStructureFalseSharing(b *testing.B) {
+	structA := SimpleStruct{}
+	structB := SimpleStruct{}
+	var wg sync.WaitGroup
+
+	runtime.GOMAXPROCS(2)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		wg.Add(2)
+		go func() {
+			defer wg.Done()
+			for j := 0; j < loopLen; j++ {
+				structA.n += j
+			}
+		}()
+		go func() {
+			defer wg.Done()
+			for j := 0; j < loopLen; j++ {
+				structB.n += j
+			}
+		}()
+		wg.Wait()
+	}
+}
+
+func BenchmarkStructureFalseSharingWithCacheLinePad(b *testing.B) {
+	structA := SimpleStruct{}
+	structB := PaddedStruct{}
+	var wg sync.WaitGroup
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		wg.Add(2)
+		go func() {
+			defer wg.Done()
+			for j := 0; j < loopLen; j++ {
+				structA.n += j
+			}
+		}()
+		go func() {
+			defer wg.Done()
+			for j := 0; j < loopLen; j++ {
+				structB.n += j
+			}
+		}()
+		wg.Wait()
+	}
+}
