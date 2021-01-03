bbf0e5dfae1dd3ff38ad6aaa209ae61ca07d60d6
Evgeny Samsonov <bitniks@yandex.ru>
2021-01-03 19:13:12 +0300

    Micro

---
+++ microservices/6_micro/6_micro.go
@@ -0,0 +1,16 @@
+package main
+
+import "github.com/micro/go-micro/v2"
+
+func main() {
+	// create a new service
+	service := micro.NewService(
+		micro.Name("helloworld"),
+	)
+
+	// initialise flags
+	service.Init()
+
+	// start the service
+	service.Run()
+}
---
+++ microservices/6_micro/go.mod
@@ -0,0 +1,5 @@
+module github.com/evsamsonov/go-learning/microservices/6_micro
+
+go 1.15
+
+require github.com/micro/go-micro/v2 v2.9.1
