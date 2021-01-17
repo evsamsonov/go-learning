1056ba04e2c98041d80b644b4869c9064f15a48b
Evgeny Samsonov <bitniks@yandex.ru>
2021-01-17 14:22:21 +0300

    Print methods

--- mastering-go/7_reflect.go
+++ mastering-go/7_reflect.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"reflect"
 )
 
@@ -23,5 +24,16 @@ func main() {
 	fmt.Printf("NumField: %d\n", ar.NumField())
 	for i := 0; i < ar.NumField(); i++ {
 		fmt.Printf("Field %d: %s\n", i, ar.Field(i).Type())
+	}
+
+	var f *os.File
+	printMethods(f)
+}
+
+func printMethods(i interface{}) {
+	r := reflect.ValueOf(i)
+	t := r.Type()
+	for j := 0; j < r.NumMethod(); j++ {
+		fmt.Printf("%s -> %s\n", t.Method(j).Name, r.Method(j).Type())
 	}
 }
