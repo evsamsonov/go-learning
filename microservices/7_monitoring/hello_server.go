a679e40ed0f52e4a241894322abc54cef6afcfc4
Evgeny Samsonov <bitniks@yandex.ru>
2021-03-11 12:21:10 +0300

    Monitoring

--- microservices/7_monitoring/hello_server.go
+++ microservices/7_monitoring/hello_server.go
@@ -3,10 +3,11 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"log"
 	"net/http"
-
-	"github.com/alexcesaro/statsd"
 )
 
 type helloRequest struct {
@@ -14,25 +15,29 @@ type helloRequest struct {
 }
 
 func main() {
-	statdClient, err := statsd.New()
-	if err != nil {
-		log.Fatalf("Failed to create statd client: %v", err)
-	}
-	defer statdClient.Close()
+	helloCalledMetric := promauto.NewCounter(prometheus.CounterOpts{
+		Name: "app_hello_called",
+	})
+	helloSuccessMetric := promauto.NewCounter(prometheus.CounterOpts{
+		Name: "app_hello_success",
+	})
+	helloFailedMetric := promauto.NewCounter(prometheus.CounterOpts{
+		Name: "app_hello_failed",
+	})
 
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		defer statdClient.NewTiming().Send("hello.response_time")
-		statdClient.Increment("hello.called")
+		helloCalledMetric.Inc()
 
 		var hello helloRequest
 		err := json.NewDecoder(r.Body).Decode(&hello)
 		if err != nil {
-			statdClient.Increment("hello.failed")
+			helloFailedMetric.Inc()
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
 		fmt.Fprintf(w, "Hello, %s!\n", hello.Name)
-		statdClient.Increment("hello.success")
+		helloSuccessMetric.Inc()
 	})
+	http.Handle("/metrics", promhttp.Handler())
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
