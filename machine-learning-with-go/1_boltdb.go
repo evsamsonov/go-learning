c35c32f3296ea15c28c184bfd2d0b9e422d73388
Evgeny Samsonov <bitniks@yandex.ru>
2021-08-23 00:28:11 +0300

    Update 1_boltdb.go

--- machine-learning-with-go/1_boltdb.go
+++ machine-learning-with-go/1_boltdb.go
@@ -13,20 +13,20 @@ func main() {
 	}
 	defer db.Close()
 
-	//err = db.Update(func(tx *bolt.Tx) error {
-	//	bucket, err := tx.CreateBucket([]byte("MyBucket"))
-	//	if err != nil {
-	//		return fmt.Errorf("create bucket: %w", err)
-	//	}
-	//
-	//	if err = bucket.Put([]byte("key1"), []byte("value1")); err != nil {
-	//		return fmt.Errorf("put: %w", err)
-	//	}
-	//	return nil
-	//})
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
+	err = db.Update(func(tx *bolt.Tx) error {
+		bucket, err := tx.CreateBucket([]byte("MyBucket"))
+		if err != nil {
+			return fmt.Errorf("create bucket: %w", err)
+		}
+	
+		if err = bucket.Put([]byte("key1"), []byte("value1")); err != nil {
+			return fmt.Errorf("put: %w", err)
+		}
+		return nil
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	err = db.View(func(tx *bolt.Tx) error {
 		bucket := tx.Bucket([]byte("MyBucket"))
