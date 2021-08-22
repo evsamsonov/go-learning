1e632d24384238a9816c37b3072884f493a31784
Evgeny Samsonov <bitniks@yandex.ru>
2021-08-22 13:12:36 +0300

    Try boltdb

---
+++ machine-learning-with-go/1_boltdb.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"github.com/boltdb/bolt"
+	"log"
+)
+
+func main() {
+	db, err := bolt.Open("machine-learning-with-go/bolt.db", 0600, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	//err = db.Update(func(tx *bolt.Tx) error {
+	//	bucket, err := tx.CreateBucket([]byte("MyBucket"))
+	//	if err != nil {
+	//		return fmt.Errorf("create bucket: %w", err)
+	//	}
+	//
+	//	if err = bucket.Put([]byte("key1"), []byte("value1")); err != nil {
+	//		return fmt.Errorf("put: %w", err)
+	//	}
+	//	return nil
+	//})
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+
+	err = db.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte("MyBucket"))
+
+		err := bucket.ForEach(func(k, v []byte) error {
+			fmt.Printf("%s: %s\n", string(k), string(v))
+			return nil
+		})
+		if err != nil {
+			return fmt.Errorf("foreach: %w", err)
+		}
+		return nil
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
