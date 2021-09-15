47a120c8feab9ed2bd287d7edbf9cbd2980cd21a
Evgeny Samsonov <bitniks@yandex.ru>
2021-09-15 23:16:54 +0300

    Measures of central tendency

---
+++ machine-learning-with-go/2_measures_of_central_tendency.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"github.com/kniren/gota/dataframe"
+	"github.com/montanaflynn/stats"
+	"gonum.org/v1/gonum/stat"
+	"log"
+	"os"
+)
+
+func main() {
+	irisFile, err := os.Open("machine-learning-with-go/data/iris.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer irisFile.Close()
+
+	irisDataframe := dataframe.ReadCSV(irisFile)
+	sepalLength := irisDataframe.Col("sepal_length")
+
+	fmt.Printf("series = %v\n\n", sepalLength.Float())
+
+	meanVal := stat.Mean(sepalLength.Float(), nil)
+	fmt.Printf("mean = %f\n", meanVal)
+
+	modeVal, modeCount := stat.Mode(sepalLength.Float(), nil)
+	fmt.Printf("mode = %f, mode count = %f\n", modeVal, modeCount)
+
+	medianVal, err := stats.Median(sepalLength.Float())
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("median = %f\n", medianVal)
+}
