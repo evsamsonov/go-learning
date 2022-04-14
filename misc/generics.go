d7d073587298145c4ea61b6490a7d6845620c26b
Evgeny Samsonov <bitniks@yandex.ru>
2022-04-14 14:36:15 +0300

    Generics example

---
+++ misc/generics.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	m := map[string]int{
+		"a": 1,
+		"b": 2,
+	}
+	keys := MapKeys[string, int](m)
+	fmt.Println(keys)
+}
+
+func MapKeys[K comparable, V any](m map[K]V) []K {
+	var s []K
+	for k := range m {
+		s = append(s, k)
+	}
+	return s
+}
